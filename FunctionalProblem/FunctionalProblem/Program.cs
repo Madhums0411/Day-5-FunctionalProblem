b590065d91d85ff1053dde98e1fd4187d6b7ec6b
madhums0411 <madhums41175@gmail.com>
2022-07-26 19:10:56 +0530

    Flip coin to print the Percentage of head and tails

--- FunctionalProblem/FunctionalProblem/Program.cs
+++ FunctionalProblem/FunctionalProblem/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Welcome to Functional Programming Problem");
+﻿using FunctionalProblem;
+
+Console.WriteLine("Welcome to Functional Programming Problem");
+
+Console.WriteLine("Flip the coin to calculate the head and tail percentage");
+
+FlipCoin flipCoin = new FlipCoin();
+flipCoin.Flip_Percentage();
